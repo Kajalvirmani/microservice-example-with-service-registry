2f06e178332774b9136e906190138f01ced0329d
kajal <kajalvirmani24@gmail.com>
2024-05-13 18:15:00 +0530

    add circuit breaker and okta configurations

--- OrderService/src/main/java/com/exampleMicroservice/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/exampleMicroservice/OrderService/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.exampleMicroservice.OrderService.external.client;
 
+import com.exampleMicroservice.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name="external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/product")
 public interface ProductService {
 
@@ -13,4 +16,8 @@ public interface ProductService {
     ResponseEntity<Void> reduceQuantity(
             @PathVariable("id")long productId,
             @RequestParam long quantity);
+
+    default void fallback(Exception e){
+        throw new CustomException("Product service is not available","UNAVAILABLE",500);
+    }
 }
